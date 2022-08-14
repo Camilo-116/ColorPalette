194e4b846e1b51726a890f364cd7ae283bd74fb0
Camilo <cacespedes@uninorte.edu.co>
2022-08-14 17:12:14 -0500

    Color palette

--- lib/ui/pages/color_selection_page.dart
+++ lib/ui/pages/color_selection_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter/services.dart';
@@ -25,9 +27,57 @@ class ColorSelectionPage extends StatelessWidget {
           ],
         ),
         // https://colorhunt.co/
-        body: (const Center(
-          child: Text(
-              "SingleChildScrollView -> Column -> [ColorPalette, ColorPalette] "),
+        body: (Align(
+          alignment: Alignment.topCenter,
+          child: SingleChildScrollView(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              ColorPalette(
+                  baseColor: '293462',
+                  baseColor1: 'D61C4E',
+                  baseColor2: 'FEB139',
+                  baseColor3: 'FFF80A',
+                  callback: showColor),
+              ColorPalette(
+                  baseColor: '554994',
+                  baseColor1: 'F675A8',
+                  baseColor2: 'F29393',
+                  baseColor3: 'FFCCB3',
+                  callback: showColor),
+              ColorPalette(
+                  baseColor: '820000',
+                  baseColor1: 'B9005B',
+                  baseColor2: 'FF7C7C',
+                  baseColor3: 'FEE0C0',
+                  callback: showColor),
+              ColorPalette(
+                  baseColor: '002B5B',
+                  baseColor1: '2B4865',
+                  baseColor2: '256D85',
+                  baseColor3: '8FE3CF',
+                  callback: showColor),
+              ColorPalette(
+                  baseColor: '100720',
+                  baseColor1: '31087B',
+                  baseColor2: 'FA2FB5',
+                  baseColor3: 'FFC23C',
+                  callback: showColor),
+              ColorPalette(
+                  baseColor: '100F0F',
+                  baseColor1: '0F3D3E',
+                  baseColor2: 'E2DCC8',
+                  baseColor3: 'F1F1F1',
+                  callback: showColor),
+              ColorPalette(
+                  baseColor: '5800FF',
+                  baseColor1: '0096FF',
+                  baseColor2: '00D7FF',
+                  baseColor3: '72FFFF',
+                  callback: showColor),
+            ],
+          )),
         )));
   }
 
--- lib/ui/widgets/color_palette.dart
+++ lib/ui/widgets/color_palette.dart
@@ -2,18 +2,23 @@ import 'package:flutter/material.dart';
 import '../../utils/color_utils.dart';
 
 class ColorPalette extends StatelessWidget {
-  const ColorPalette({
-    Key? key,
-    // así se reciben en el constructor, por ejemplo
-    //required this.baseColor,
-  }) : super(key: key);
+  const ColorPalette(
+      {Key? key,
+      // así se reciben en el constructor, por ejemplo
+      //required this.baseColor,
+      required this.baseColor,
+      required this.baseColor1,
+      required this.baseColor2,
+      required this.baseColor3,
+      required this.callback})
+      : super(key: key);
 
   // atributos de la clase, por ejemplo
-  //final String baseColor;
+  final String baseColor, baseColor1, baseColor2, baseColor3;
 
   // también se puede tener como un atributo un callback, necesiario ya que es un
   // widget sin estado, por ejemplo
-  //final Function(String) callback;
+  final Function(String) callback;
 
   @override
   Widget build(BuildContext context) {
@@ -21,8 +26,36 @@ class ColorPalette extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       // vamos a mostrar los elementos en un row
-      child: Text(
-          'Row -> [Expanded(flex: 4,GestureDetector(Container(height: 100, color: ColorUtils.FromHex(baseColor))) )]'),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Expanded(
+              flex: 4,
+              child: GestureDetector(
+                  onTap: () => callback(baseColor),
+                  child: Container(
+                      height: 100, color: ColorUtils.FromHex(baseColor)))),
+          Expanded(
+              flex: 3,
+              child: GestureDetector(
+                  onTap: () => callback(baseColor1),
+                  child: Container(
+                      height: 100, color: ColorUtils.FromHex(baseColor1)))),
+          Expanded(
+              flex: 2,
+              child: GestureDetector(
+                  onTap: () => callback(baseColor2),
+                  child: Container(
+                      height: 100, color: ColorUtils.FromHex(baseColor2)))),
+          Expanded(
+              flex: 1,
+              child: GestureDetector(
+                  onTap: () => callback(baseColor3),
+                  child: Container(
+                      height: 100, color: ColorUtils.FromHex(baseColor3)))),
+        ],
+      ),
     );
   }
 }
